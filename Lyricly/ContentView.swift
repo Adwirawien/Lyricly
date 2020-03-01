5410a23fbd3274687893f0d04dbb293c197f6581
Adwirawien <hannsadrian@gmail.com>
2020-03-01 14:27:25 +0100

    Initial Commit

---
+++ Lyricly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lyricly
+//
+//  Created by Adrian Böhme on 01.03.20.
+//  Copyright © 2020 Hanns Adrian Böhme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
