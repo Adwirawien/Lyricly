f458c4cc335b1a13b7416095723e47de3100919f
Adwirawien <hannsadrian@gmail.com>
2020-03-01 21:56:26 +0100

    Update layout

--- Lyricly/ContentView.swift
+++ Lyricly/ContentView.swift
@@ -15,11 +15,7 @@ struct ContentView: View {
     @State var isModalActive: Bool = true
 
     func change() {
-        print("uwu")
-    }
-
-    func commit() {
-        print("commited!")
+        print(song)
     }
 
     var body: some View {
@@ -27,56 +23,38 @@ struct ContentView: View {
             VStack {
                 VStack {
                     Caption(name: "SONG")
-                        .padding(.top, 20)
-                        .padding(.bottom, -22.5)
-                    TextField("Lone Digger", text: $song)
+                        .padding(.top, CGFloat(20))
+                        .padding(.bottom, CGFloat(-22.5))
+                    TextField("Lone Digger", text: Binding(
+                        get: {
+                            self.song
+                        },
+                        set: { (newValue) in
+                            self.song = newValue
+                            self.change()
+                        }
+                        ))
                         .padding()
-                        .padding(.top, 0)
-                        .padding(.bottom, 0)
+                        .padding(.top, CGFloat(0))
+                        .padding(.bottom, CGFloat(0))
                         .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .shadow(color: colorScheme == .dark ? Color.gray.opacity(0) : Color.gray.opacity(0.2), radius: 10, y: 5)
-
-                    Caption(name: "ARTIST")
-                        .padding(.top, -12.5)
-                        .padding(.bottom, -50)
-                    TextField("Caravan Palace", text: $artist)
-                        .padding()
-                        .padding(.top, -20)
-                        .padding(.bottom, 0)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .shadow(color: colorScheme == .dark ? Color.gray.opacity(0) : Color.gray.opacity(0.2), radius: 10, y: 5)
+                        .shadow(color: colorScheme == .dark ? Color.gray.opacity(0.0) : Color.gray.opacity(0.2), radius: CGFloat(10), y: CGFloat(5))
                 }
-                Button(action: { self.isModalActive.toggle() }) {
-                    HStack {
-                        Spacer()
-                        Text("Search Song")
-                            .padding(.vertical, 12.5)
-                        Spacer()
-                    }
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(7.5)
-                        .padding(.horizontal)
-                        .padding(.top, 10)
-                }.sheet(isPresented: $isModalActive, content: {
-                    NavigationView {
-                        Text("hello world!")
-                            .navigationBarTitle(self.song)
-                            .navigationBarItems(trailing:
-                                    Button(action: { self.isModalActive.toggle() }) {
-                                        Text("Close")
-                                }
-                            )
-                    }
-                })
                 Spacer()
-                Image("image")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.horizontal, 20)
+                VStack {
+                    Image("image")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.horizontal, CGFloat(20))
+                    Text("🎙 Find the right lyrics")
+                        .font(.system(size: 15, weight: .regular, design: .rounded))
+                        .foregroundColor(Color.gray.opacity(0.8))
+                        .padding(.top, -20)
+                }
+                .padding(.bottom, 80)
                 Spacer()
             }
-                .navigationBarTitle("Lyricly")
+                .navigationBarTitle(song != "" ? song : "Lyricly")
         }
     }
 }
